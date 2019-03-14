dfe82392a3bb2d737d8674f8baeb19dcd28398fb
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-03-14 14:08:15 +0100

    add gaf imports at module level

--- waterbalans/__init__.py
+++ waterbalans/__init__.py
@@ -1,6 +1,7 @@
-from .buckets import Verhard, Onverhard, Drain
-from .create import create_eag
+from .buckets import Verhard, Onverhard, Drain, MengRiool
+from .create import create_eag, create_gaf
 from .eag import Eag
+from .gaf import Gaf
 from .timeseries import get_series
 from .water import Water
 from .utils import *
