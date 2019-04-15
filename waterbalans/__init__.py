edc7c89e5c28882e042bb3367af4d49938ff09f8
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-04-15 10:37:20 +0200

    add import update_series

--- waterbalans/__init__.py
+++ waterbalans/__init__.py
@@ -2,6 +2,6 @@ from .buckets import Verhard, Onverhard, Drain, MengRiool
 from .create import create_eag, create_gaf
 from .eag import Eag
 from .gaf import Gaf
-from .timeseries import get_series
+from .timeseries import get_series, update_series
 from .water import Water
 from .utils import *
