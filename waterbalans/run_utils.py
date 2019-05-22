827febad75190d83ede4ca6456b1847e96a013e4
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-05-22 16:28:47 +0200

    add run utilitites

---
+++ waterbalans/run_utils.py
@@ -0,0 +1,88 @@
+import os
+
+import pandas as pd
+
+from .create import create_eag
+from .utils import create_csvfile_table, add_timeseries_to_obj
+
+
+def run_eag_by_name(name, csvdir="./data/input_csv", tmin="1996", tmax="2019"):
+    file_df = create_csvfile_table(csvdir)
+    fbuckets, fparams, freeks, fseries, _, _ = file_df.loc[name]
+
+    dfdict = get_dataframes_from_files(fbuckets=fbuckets, fparams=fparams, freeks=freeks,
+                                       fseries=fseries, csvdir=csvdir)
+
+    deelgebieden = dfdict["deelgebieden"]
+    tijdreeksen = dfdict["tijdreeksen"]
+    parameters = dfdict["parameters"]
+    series = dfdict["series"]
+
+    # %% Simulation settings based on parameters
+    # ------------------------------------------
+    if parameters.loc[parameters.ParamCode == "hTargetMin", "Waarde"].iloc[0] != -9999.:
+        use_wl = True
+    else:
+        use_wl = False
+
+    # %% Model
+    # --------
+    # Maak bakjes model
+    e = create_eag(name.split("-")[-1], name, deelgebieden,
+                   use_waterlevel_series=use_wl,
+                   logfile="waterbalans.log")
+
+    # Voeg tijdreeksen toe
+    e.add_series_from_database(tijdreeksen, tmin=tmin, tmax=tmax)
+
+    # Voeg overige tijdreeksen toe (overschrijf FEWS met Excel)
+    if series is not None:
+        add_timeseries_to_obj(e, series, overwrite=True)
+
+    # Force MengRiool to use external timeseries
+    mengriool = e.get_buckets(buckettype="MengRiool")
+    if len(mengriool) > 0:
+        for b in mengriool:
+            b.use_eag_cso_series = False
+            b.path_to_cso_series = r"./data/cso_series/240_cso_timeseries.pklz"
+
+    # Simuleer waterbalans met parameters
+    e.simulate(parameters, tmin=tmin, tmax=tmax)
+
+    return e
+
+
+def get_dataframes_from_files(fbuckets=None, freeks=None, fparams=None,
+                              fseries=None, csvdir="./data/input_csv"):
+    dflist = {}
+    if fbuckets is not None:
+        # bestand met deelgebieden en oppervlaktes:
+        deelgebieden = pd.read_csv(
+            os.path.join(csvdir, fbuckets), delimiter=";")
+        dflist["deelgebieden"] = deelgebieden
+    if freeks is not None:
+        # bestand met tijdreeksen, b.v. neerslag/verdamping:
+        tijdreeksen = pd.read_csv(os.path.join(csvdir, freeks), delimiter=";")
+        dflist["tijdreeksen"] = tijdreeksen
+    if fparams is not None:
+        # bestand met parameters per deelgebied
+        parameters = pd.read_csv(os.path.join(csvdir, fparams), delimiter=";")
+        dflist["parameters"] = parameters
+    if fseries is not None:
+        # bestand met overige tijdreeksen
+        if not isinstance(fseries, float):
+            series = pd.read_csv(os.path.join(csvdir, fseries),
+                                 delimiter=";", index_col=[0], parse_dates=True)
+        else:
+            series = None
+        dflist["series"] = series
+    return dflist
+
+
+def get_dataframes_by_name(name, csvdir="./data/input_csv"):
+    file_df = create_csvfile_table(csvdir)
+    fbuckets, fparams, freeks, fseries, _, _ = file_df.loc[name]
+
+    dfdict = get_dataframes_from_files(fbuckets=fbuckets, fparams=fparams, freeks=freeks,
+                                       fseries=fseries, csvdir=csvdir)
+    return dfdict
